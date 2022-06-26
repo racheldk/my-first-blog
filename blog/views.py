1fda425d4f570f5906fda69a7b1febee28d581c0
Rachel Kelly <racheldk126@gmail.com>
2022-06-26 11:58:56 -0700

    Added form - not  yet loading on page

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+
+from djangogirls.blog.forms import PostForm
 from .models import Post
 
 # Create your views here.
@@ -10,3 +12,7 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})    
+
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
