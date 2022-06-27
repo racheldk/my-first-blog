6cc6cdacfdda791bb839ed202df60dd8dcfed10e
Rachel Kelly <racheldk126@gmail.com>
2022-06-26 19:42:36 -0700

    debugged

--- blog/views.py
+++ blog/views.py
@@ -15,16 +15,17 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})    
 
 def post_new(request):
-    if request.method =="POST":
+    if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
-            post.save()     
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm()    
-    return redirect('post_detail', pk=post.pk)    
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})   
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -38,4 +39,4 @@ def post_edit(request, pk):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})          
+    return render(request, 'blog/post_edit.html', {'form': form})       
